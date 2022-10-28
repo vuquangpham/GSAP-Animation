bf01f87f986b523399076895d1291dd4dab939db
vupham <vuquangpham2909@gmail.com>
2022-10-28 15:08:35 +0700

    :zap: adjust style and reformat code

--- src/_index.js
+++ src/_index.js
@@ -1,49 +1,44 @@
 import 'assets/style/style.css';
 import gsap from 'gsap';
 import ScrollTrigger from "gsap/ScrollTrigger";
-import {Linear} from 'gsap';
 
 gsap.registerPlugin(ScrollTrigger);
 
 window.addEventListener('load', () => {
-
     const images = gsap.utils.toArray(".box");
-    const img = images[0];
-    img.firstElementChild.style.width = `${img.getBoundingClientRect().width}px`;
-    img.firstElementChild.style.height = `${img.getBoundingClientRect().height}px`;
+
+    // set width and height for image
 
     const tl = gsap.timeline({
         scrollTrigger: {
             trigger: document.querySelector('.wrapper'),
-            // pin: true,
             scrub: true,
             markers: true,
             start: '+=200px',
-            end: '+=400px',
+            end: '+=350px',
         },
-        ease: "none"
+        defaults: {
+            ease: "linear"
+        }
     });
-    tl.addLabel('delay');
     tl.to(images, {
         x: '0',
         y: '0',
         rotate: '0',
-        ease: "none"
     });
+
+    tl.addLabel('delay', '-=0.15');
     tl.to(images[0], {
         height: `98px`,
-        ease: "none"
-    }, 'delay+=0.35');
+    }, 'delay');
     tl.to(images[3], {
         width: `193px`,
         height: '290px',
         borderRadius: '0 60px 0 0',
-        ease: "none"
-    }, 'delay+=0.35');
+    }, 'delay');
     tl.to(images[4], {
         width: `193px`,
         height: '136px',
         borderRadius: '0 0 60px 0',
-        ease: "none"
-    }, 'delay+=0.35d');
+    }, 'delay');
 });
